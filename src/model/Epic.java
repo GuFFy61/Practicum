fbc4d51cb091b9c39410e806dae1659fb8e8977f
Igor Khodyrev <khodyrev.ig@gmail.com>
2022-12-22 18:09:44 +0300

    feat: создать класс для описания сущности задачи типа "эпик"

---
+++ src/model/Epic.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    ArrayList<SubTask> subTasks = new ArrayList<>();
+
+    public Epic(String type, String title, String description, Integer id, Status status) {
+        super(type, title, description, id, status);
+    }
+
+    public Epic(String name, String description, Integer id) {
+        super(name, description, id);
+    }
+
+    public ArrayList<SubTask> getSubTasks() {
+        return subTasks;
+    }
+
+    public void setSubTasks(ArrayList<SubTask> subTasks) {
+        this.subTasks = subTasks;
+    }
+    public LocalDateTime getEndTime() {
+        for (SubTask subTask : subTasks) {
+            endTime.plusMinutes(subTask.getDuration());
+        }
+        return endTime;
+    }
+}
