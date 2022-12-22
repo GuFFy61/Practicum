4de562f14b6e5edac472b465ef4630152fff27f0
Igor Khodyrev <khodyrev.ig@gmail.com>
2022-12-22 17:36:58 +0300

    feat: создать класс, описывающий сущность задачи типа "подзадача"

---
+++ src/Model/SubTask.java
@@ -0,0 +1,49 @@
+package Model;
+
+import java.util.Objects;
+
+public class SubTask extends Task {
+    private Integer epicID;
+
+    public SubTask(String name, String description, Integer id, Status status, String type, Integer epicID) {
+        super(name, description, id, status, type);
+        this.epicID = epicID;
+    }
+
+    public SubTask(String name, String description, Integer id, Integer epicID) {
+        super(name, description, id);
+        this.epicID = epicID;
+    }
+
+    public Integer getEpicID() {
+        return epicID;
+    }
+
+    public void setEpicID(Integer epicID) {
+        this.epicID = epicID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SubTask)) return false;
+        if (!super.equals(o)) return false;
+        SubTask subTask = (SubTask) o;
+        return Objects.equals(getEpicID(), subTask.getEpicID());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getEpicID());
+    }
+
+    @Override
+    public String toString() {
+        return type +
+                ", " + name +
+                ", " + description +
+                ", " + id +
+                ", " + status +
+                ", " + epicID;
+    }
+}
